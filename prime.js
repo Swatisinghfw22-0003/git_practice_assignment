492ea327837019d67301a30e2239fa36793db15b
Swati singh <swativishal8266@gmail.com>
2022-09-20 16:39:12 +0530

    added palindrome problem

--- prime.js
+++ prime.js
@@ -1,3 +1,4 @@
+//prime num or not
 let num=8;
 let count=0;
 for(let i=0;i<=6;i++){
@@ -7,4 +8,15 @@ if(count==2){
 console.log("prime")
 }esle {
 console.log("not a prime"}
+}
+ //a palindrome or note
+let str = "naman";
+let bag = "";
+for(let i = str.length-1;i>=0;i--){
+    bag = bag + str[i];
+}
+if(str === bag){
+    console.log(str,"is a palindrome");
+}else{
+    console.log(str,"is not a palindrome");
 }
